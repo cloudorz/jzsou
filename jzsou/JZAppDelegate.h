a889ffcf7efd9bf4b472de83acd37bd0ee93872d
cloud <cloudcry@gmail.com>
2012-03-17 11:03:47 +0800

    Initial Commit

---
+++ jzsou/JZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JZAppDelegate.h
+//  jzsou
+//
+//  Created by Dai Cloud on 12-3-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ jzsou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  jzsou
+//
+//  Created by Dai Cloud on 12-3-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JZAppDelegate class]));
+    }
+}
